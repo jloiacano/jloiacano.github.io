521b4b0d92dd3e89b1349651ea44842505211818
jloiacano <j.loiacano55@gmail.com>
2017-10-11 12:27:13 -0500

    added the base of some javascript, and some possible background imagry in large format

---
+++ js/pageing.js
@@ -0,0 +1,24 @@
+//get the window width
+
+// set "page" points based on multiples of window width
+
+// apply "page" points to be moved to when user clicks anchors
+
+// slide the allPages <div>
+
+/*
+var pageWidth = document.body.clientWidth;
+
+var currentPage = 1;
+var pageToGoTo = 1;
+*/
+
+function loadThePage() {
+    "use strict";
+    window.alert(document.body.clientWidth);
+}
+
+window.onload = function () {
+    "use strict";
+    loadThePage();
+};
