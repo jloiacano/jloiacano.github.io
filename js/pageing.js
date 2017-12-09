2db73a55f009164befcb70b87aa9c032043dc75a
jloiacano <j.loiacano55@gmail.com>
2017-12-09 11:24:49 -0600

    Change of design idea. Off to a new destination!

--- js/pageing.js
+++ js/pageing.js
@@ -1,34 +1,69 @@
-//get the window width
-
-// set "page" points based on multiples of window width
-
-// apply "page" points to be moved to when user clicks anchors
-
-// slide the allPages <div>
-
-var pageWidth;
-var currentPage;
-
-document.getElementById("home").addEventListener("click", function() { goToPage(1) });
-document.getElementById("about").addEventListener("click", function() { goToPage(2) });
-document.getElementById("talents").addEventListener("click", function() { goToPage(3) });
-document.getElementById("contact").addEventListener("click", function() { goToPage(4) });
-
-function goToPage(page) {
+(function () {
     "use strict";
-    pageWidth = document.body.clientWidth;
-    currentPage = page;
-    var pointToGoTo = (currentPage - 1) * pageWidth;
-    window.scrollTo(pointToGoTo, 0);
-}
 
-function loadThePage() {
-    "use strict";
-    pageWidth = document.body.clientWidth;
-    currentPage = 1;
-}
+    var pageWidth, currentPage;
 
-window.onload = function () {
-    "use strict";
-    loadThePage();
-};
+    window.aBlankFunction = function (where) {
+        console.log("aBlankFunction() called from " + where);
+    };
+
+    window.delayTheVariableChange = function (number, iOrD) {
+        if (iOrD === "i") {
+            number += 1;
+            return number;
+        } else if (iOrD === "d") {
+            number -= 1;
+            return number;
+        } else {
+            console.log("issue with iOrD in delayTheVariableChange");
+            return number;
+        }
+    };
+
+    window.goToPage = function (page) {
+        console.log("goToPage() started");
+        pageWidth = document.body.clientWidth;
+        var currentX = ((currentPage - 1) * pageWidth),
+            pointToGoTo = ((page - 1) * pageWidth);
+        currentPage = page;
+        console.log("currentX = " + currentX + "\npointToGoTo = " + pointToGoTo);
+        while (currentX !== pointToGoTo) {
+            console.log("goToPage while loop started.");
+            if (currentX > pointToGoTo) {
+                currentX = window.setTimeout(window.delayTheVariableChange(currentX, 'd'), 100);
+                console.log("currentX: " + currentX);
+                window.scrollTo(currentX, 0);
+            } else if (currentX < pointToGoTo) {
+                currentX = window.setTimeout(window.delayTheVariableChange(currentX, 'i'), 100);
+                window.setTimeout(window.aBlankFunction('setTimeout'), 100);
+                console.log("currentX: " + currentX);
+                window.scrollTo(currentX, 0);
+            } else {
+                console.log("currentX and pointToGoTo are equal");
+            }
+        }
+    };
+
+    document.getElementById("home").addEventListener("click", function () {
+        window.goToPage(1);
+    });
+    document.getElementById("about").addEventListener("click", function () {
+        window.goToPage(2);
+    });
+    document.getElementById("talents").addEventListener("click", function () {
+        window.goToPage(3);
+    });
+    document.getElementById("contact").addEventListener("click", function () {
+        window.goToPage(4);
+    });
+
+    function loadThePage() {
+        pageWidth = document.body.clientWidth;
+        currentPage = 1;
+    }
+
+    window.onload = function () {
+        loadThePage();
+    };
+
+}());
